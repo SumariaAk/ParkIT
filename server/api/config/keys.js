974891902f4a7b6831bec0fad36b4fd30ec91217
SumariaAk <sumaria.ak@northeastern.edu>
2022-02-24 02:20:13 -0500

    Update keys.js

--- server/api/config/keys.js
+++ server/api/config/keys.js
@@ -1,5 +1,2 @@
-// module.exports={
-//     SENDGRID_API: "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",
-// }
-const SENDGRID_API = "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";
+const SENDGRID_API = "YOUR_API_KEY";
 export default SENDGRID_API;
